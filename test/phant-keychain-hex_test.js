bff9f57691a0cf6a62b5c3575c64fec7cf65c60e
Todd Treece <toddtreece@gmail.com>
2014-04-28 15:13:33 -0400

    adds a couple tests

--- test/phant-keychain-hex_test.js
+++ test/phant-keychain-hex_test.js
@@ -1,15 +1,27 @@
 'use strict';
 
-var phantKeychainHex = require('../lib/phant-keychain-hex.js');
+var Keychain = require('../lib/phant-keychain-hex.js'),
+    keys = Keychain({
+      publicSalt: 'testing',
+      privateSalt: 'testing private',
+      deleteSalt: 'testing delete'
+    });
 
 exports.phantKeychainHex = {
   setUp: function(done) {
-    // setup here
+    this.id = '123abef222cc';
     done();
   },
-  'no args': function(test) {
-    test.expect(1);
-    test.ok(phantKeychainHex, 'should be ok');
+  'private key and public key': function(test) {
+    test.expect(2);
+    test.ok(keys.validate(keys.publicKey(this.id), keys.privateKey(this.id)), 'should match');
+    test.ok(keys.validate(keys.publicKey(this.id), '1') === false, 'should not match');
+    test.done();
+  },
+  'delete key and public key': function(test) {
+    test.expect(2);
+    test.ok(keys.validateDeleteKey(keys.publicKey(this.id), keys.deleteKey(this.id)), 'should match');
+    test.ok(keys.validateDeleteKey(keys.publicKey(this.id), '1') === false, 'should not match');
     test.done();
   }
 };
